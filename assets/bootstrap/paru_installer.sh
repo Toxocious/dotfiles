6c693e6401aa4b2bef1154707946916e43e00b28
Jess <Toxocious@gmail.com>
2024-05-22 22:49:24 -0700

    Install Python and Rustup before installing Paru

--- assets/bootstrap/paru_installer.sh
+++ assets/bootstrap/paru_installer.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env sh
 
+# Install Python
+if ! pacman -Qi python &> /dev/null; then
+    echo "Python is not installed. Installing..."
+    sudo pacman -S python
+fi
+
+# Install Rustup
+if ! pacman -Qi rustup &> /dev/null; then
+    echo "Rustup is not installed. Installing..."
+    sudo pacman -S rustup
+
+    # Set default
+    rustup default stable
+fi
+
+# Now install Paru
 if command -v paru >/dev/null 2>&1;  then
   echo "paru is already installed"
 else
